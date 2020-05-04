0e5241a01a24fc331d62cb0c7e75bd84f151e583
Stefan Zdravkov <szdravkov93@gmail.com>
2020-05-04 20:41:58 +0200

    part 1

--- App.js
+++ App.js
@@ -1,114 +1,125 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {View, SafeAreaView, Text, StyleSheet} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {ChannelList} from './src/components/ChannelList';
+import {StreamChat} from 'stream-chat';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+  Chat,
+  MessageList,
+  MessageInput,
+  Channel,
+} from 'stream-chat-react-native';
+import {ChannelHeader} from './src/components/ChannelHeader';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {DateSeparator} from './src/components/DateSeparator';
+import {MessageSlack} from './src/components/MessageSlack';
+import {InputBox} from './src/components/InputBox';
 
-const App: () => React$Node = () => {
+import streamChatTheme from './src/stream-chat-theme.js';
+
+const chatClient = new StreamChat('q95x9hkbyd6p');
+const userToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidmlzaGFsIn0.LpDqH6U8V8Qg9sqGjz0bMQvOfWrWKAjPKqeODYM0Elk';
+const user = {
+  id: 'vishal',
+  name: 'Vishal',
+};
+
+chatClient.setUser(user, userToken);
+
+function ChannelScreen({navigation, route}) {
+  const [channel, setChannel] = useState(null);
+  useEffect(() => {
+    if (!channel) {
+      navigation.openDrawer();
+    }
+    const channelId = route.params ? route.params.channelId : null;
+    const _channel = chatClient.channel('messaging', channelId);
+    setChannel(_channel);
+  }, [route.params]);
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={styles.channelScreenSaveAreaView}>
+      <View style={styles.channelScreenContainer}>
+        <ChannelHeader
+          navigation={navigation}
+          channel={channel}
+          client={chatClient}
+        />
+        <View style={styles.chatContainer}>
+          <Chat client={chatClient} style={streamChatTheme}>
+            <Channel channel={channel}>
+              <MessageList
+                Message={MessageSlack}
+                DateSeparator={DateSeparator}
+              />
+              <MessageInput
+                Input={InputBox}
+                additionalTextInputProps={{
+                  placeholderTextColor: '#979A9A',
+                  placeholder:
+                    channel && channel.data.name
+                      ? 'Message #' +
+                        channel.data.name.toLowerCase().replace(' ', '_')
+                      : 'Message',
+                }}
+              />
+            </Channel>
+          </Chat>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const ChannelListDrawer = (props) => {
+  return (
+    <SafeAreaView>
+      <ChannelList
+        client={chatClient}
+        changeChannel={(channelId) => {
+          props.navigation.jumpTo('ChannelScreen', {
+            channelId,
+          });
+        }}
+      />
+    </SafeAreaView>
   );
 };
 
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Drawer.Navigator
+          drawerContent={ChannelListDrawer}
+          drawerStyle={styles.drawerNavigator}>
+          <Drawer.Screen name="ChannelScreen" component={ChannelScreen} />
+        </Drawer.Navigator>
+      </View>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  channelScreenSafeAreaView: {
+    backgroundColor: 'white',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  channelScreenContainer: {
+    flexDirection: 'column',
+    height: '100%',
   },
-  body: {
-    backgroundColor: Colors.white,
+  container: {flex: 1},
+  drawerNavigator: {
+    backgroundColor: '#3F0E40',
+    width: 350,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  chatContainer: {
+    backgroundColor: 'white',
+    flexGrow: 1,
+    flexShrink: 1,
   },
 });
-
-export default App;
